9ef73e739cd838d8174d9324b2a963391a6b0bcc
PsychedelicMonkey <ryan.getz.96@gmail.com>
2022-09-09 10:41:35 -0600

    Redux setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
-import type { NextPage } from 'next';
+import type { GetServerSideProps, NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
+import api from '../lib/api';
 
 const Home: NextPage = () => {
   return (
